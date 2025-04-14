e17d7fb42fd15ac23982c925b141efc6c16cd251
kelingchu0302 <kelingchu0302@gmail.com>
2025-04-14 12:33:33 +0800

    上傳測試

--- database_link.php
+++ database_link.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 header("Access-Control-Allow-Origin: *"); // 允許所有來源的請求
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
@@ -19,3 +20,26 @@ if ($conn->connect_error) {
 }
 
 ?>
+=======
+<?php
+header("Access-Control-Allow-Origin: *"); // 允許所有來源的請求
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header('Content-Type: application/json');
+
+$host = "localhost";  
+$user = "root";       
+$password = "";      
+$dbname = "department_transfer";  // 資料庫名稱
+
+//連線
+$conn = new mysqli($host, $user, $password, $dbname);
+
+// 檢查連線是否成功
+if ($conn->connect_error) {
+    die("連線失敗: " . $conn->connect_error);
+} else {
+}
+
+?>
+>>>>>>> 91c4379 (上傳測試)
